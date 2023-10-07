c350639d2c307dc916840c397b14d70d9d7b2704
shayan <shayan.officeal@gmail.com>
2023-10-07 11:35:19 -0700

    initial commit

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+
+    [SerializeField]
+    private ToggleGroup group;
+    [SerializeField]
+    private Text counter;
+
+    public void OnStart() {
+
+    	Toggle[] toggle = group.GetComponentsInChildren<Toggle>();
+	toggle[UnityEngine.Random.Range(0, toggle.Length)].isOn = true;
+	counter.text = (int.Parse(counter.text) + UnityEngine.Random.Range(0, toggle.Length)).ToString();
+    }
+    public void OnRate() {
+
+	string URL = "market://details?id=com.NETEMPYRE.Clicker";
+	Application.OpenURL(URL);
+    }
+}
